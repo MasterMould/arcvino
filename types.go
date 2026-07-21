483f47d7ec91c89355ca8f9521b524b2ada9ab11
MasterMould <techmustserve@gmail.com>
2026-07-21 18:55:24 +0100

    Add PageData struct for telemetry data
    Added PageData struct to hold hardware telemetry.

--- types.go
+++ types.go
@@ -1,5 +1,11 @@
 package main
 
+// PageData holds hardware telemetry passed into the HTML template renderer.
+type PageData struct {
+	CPU       string
+	GPUStatus string
+}
+
 // InstallOptions defines the configuration for environment deployment.
 type InstallOptions struct {
 	UseNightly bool   `json:"useNightly"`
